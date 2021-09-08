eb256663586e53fe591797d7103bf7571aa579fc
Enasone <Usovdima977@gmail.com>
2021-09-09 02:22:16 +0500

    add components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SummaryPageComponent } from './modules/summary-page/summary-page.component';
+import { ListPageComponent } from './modules/list-page/list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryPageComponent,
+    ListPageComponent
   ],
   imports: [
     BrowserModule,
