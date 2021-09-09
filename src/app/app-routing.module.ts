e1149b26f53cada45d0cdeb49ae2efc4279ce6e7
Enasone <Usovdima977@gmail.com>
2021-09-10 03:18:05 +0500

    add html and css for tabs (2.2 task)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SummaryPageComponent} from "./modules/summary-page/summary-page.component";
+import {ListPageComponent} from "./modules/list-page/list-page.component";
 
 const routes: Routes = [
-  {path: '', component: SummaryPageComponent}
+  { path: '', component: SummaryPageComponent },
+  { path: 'navigator', component: ListPageComponent}
 ];
 
 @NgModule({
